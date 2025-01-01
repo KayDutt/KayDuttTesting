a074504dc3d84477baf57b77f7c6ac747e45f18b
KayDutt <97658116+KayDutt@users.noreply.github.com>
2025-01-01 20:07:59 +0530

    1st Commit

--- DOM-manipulation.js
+++ DOM-manipulation.js
@@ -1,2 +1,8 @@
 // Example: Selecting an element with the id "example"
 let element = document.getElementById("example");
+// Example: Selecting all elements with the class "example"
+let elements = document.getElementsByClassName("example");
+// Example: Selecting all elements with the tag name "div"
+let divs = document.getElementsByTagName("div");
+// Example: Selecting the first element with the class "example"
+let firstElement = document.querySelector(".example")
